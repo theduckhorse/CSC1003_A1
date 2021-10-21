5b942577d5d565e579a04c61caef54a13facde2f
Water <hongsheng322@gmail.com>
2021-10-21 17:18:51 +0800

    Revert "Added enum types and probabilty functions"

--- Header Files/naive_bayes.h
+++ Header Files/naive_bayes.h
@@ -1,10 +1,10 @@
 /* declare your functions here */
 #ifndef NAIVE_BAYES_H
 #define NAIVE_BAYES_H
+
 #define ALPHA 20; 
 
 extern float altered_probablity, normal_probablity;
-
 struct Probablity
 {
     //alpha 4 
@@ -29,6 +29,7 @@ struct Probablity
 extern struct Probablity altered;
 extern struct Probablity normal;
 
+
 //assume that collumns are always the same data.
 
 //setup initial probablity for altered and normal | float** is a 2d array
@@ -49,22 +50,5 @@ float Gaussian_Probablity();
 //use (results*100)/total data to get scoring 
 //calculate the score for the naive bayes algo
 float Scoring();
-
-//enum class
-enum Season{winter, spring, summer, fall};
-enum Fever{less_than_three_months_ago = -1, more_than_three_months_ago, No};
-enum Alcohol_Consumption{several_times_a_day, every_day, several_times_a_week, once_a_week, hardly_ever_or_never};
-enum Smoking_Habit{never = -1, occasional, daily};
-
-//functions to calculate probability
-float Prob_Season(enum Season e);
-float Prob_Age(float age);
-float Prob_Childish_Disease(int choice);
-float Prob_Accident(int choice);
-float Prob_Surgical_Intervention(int choice);
-float Prob_Fever(enum Fever e);
-float Prob_Alcohol_Consumption(enum Alcohol_Consumption e);
-float Prob_Smoking(enum Smoking_Habit e);
-float Prob_Sitting(float duration);
 
 #endif
